bec97960bd2f48a5e8d91f6763906c56138274c9
HShaw215 <hu.shaw215@gmail.com>
2023-05-10 14:06:21 -0400

    Completed styling for each key

--- client/components/Song.jsx
+++ client/components/Song.jsx
@@ -6,7 +6,7 @@ import '../scss/containerStyles.scss';
 
 function Song(props) {
     return (
-        <div class='songDisplay'>
+        <div class={props.song.key}>
             <p>Name: {props.song.name}</p>
             <p>Key: {props.song.key}</p>
             <p>Length: {props.song.length}</p>
