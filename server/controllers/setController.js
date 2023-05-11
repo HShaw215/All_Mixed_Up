aa09382e5da2e0b658050a0deb63ffa3ff26d989
HShaw215 <hu.shaw215@gmail.com>
2023-05-11 11:55:07 -0400

    completed filtering db randomly

--- server/controllers/setController.js
+++ server/controllers/setController.js
@@ -13,22 +13,30 @@ const setController = {
         const newTimer = timer * 6;
         // console.log(newTimer);
         //try using aggregate to get random amount of docs (need to figure out how to import whole database as db)
-       res.locals.playlist = db.songs.aggregate([
-        { $sample: { size: newtimer } },
-        { $sort: { key: 1}},
-      ]);
-        // {'$match': {}}, 
-        // {'$sample': {'size': newTimer}}]));
+      try {
+        res.locals.playlist = await Song.aggregate([
+            { $sample: { size: newTimer } },
+            { $sort: { key: 1}},
+          ]);
+          return next();
+      } 
+      catch {
+        return next({
+            err: {err: "Invalid set time entry"}
+        });
+    }
+    //   console.log(res.locals.playlist)
         //now we need to make a find request to the server where somehow its a random pull of data adding up to newTimer
         // res.locals.playlist = timer;
-        return next()
-    }
+    },
 
 };
 
 //It seems like a filter for adding up certain properties doens't really exist so I might just have to assume that the average length
     //of each song is about 6 minutes. Then I can divide newTimer by 6 to find out how many songs I should pull to populate the new playlist,
     //or just make newTimer be timer * 10 instead. Still need to figure out how to create random querying 
+
+    //db.songs.aggregate
 
     //also need to import the database as db so that I can filter the aggregation on db.songs.aggregation
 
