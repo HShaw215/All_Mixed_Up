c6561aba9b955aa08f6524f5f21f203ecdb06dd1
HShaw215 <hu.shaw215@gmail.com>
2023-07-19 15:49:02 -0400

    updated status code for timerRoute

--- server/routes/timerRoute.js
+++ server/routes/timerRoute.js
@@ -3,9 +3,10 @@ const setController = require('../controllers/setController');
 
 const timerRoute = express.Router();
 
+//route will create a new playlist of songs adding up to input time value and return playlist
 timerRoute.post('/', setController.createPlaylist, (req, res) => {
     console.log('playlist generated');
-    res.status(200).send(res.locals.playlist);
+    res.status(201).send(res.locals.playlist);
 })
 
 module.exports = timerRoute;
