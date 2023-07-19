4e5d187a38d00b0da5e12cbcf69a9437c16c67ae
HShaw215 <hu.shaw215@gmail.com>
2023-07-19 15:47:14 -0400

    cleaned up setController pseudocode

--- server/controllers/setController.js
+++ server/controllers/setController.js
@@ -2,16 +2,17 @@ const songSchema = require('../models/songModel');
 
 const setController = {
 
-    //idea is to pull time from req.body, convert to minutes, then find a random pull of data from the database adding up to roughly the
-        //input time. Once that's done, then the pulled data needs to be sorted into ascending key somehow like 1A -> 1B -> 2A
+    //declare method createPlaylist that pulls random amount of sorted songs based on input timer value
     async createPlaylist(req, res, next) {
         console.log('setController createPlaylist is running');
         const { timer } = req.body;
-        //multiple input time by 60 to get total number of minutes (average time is about 7 minutes so multiply timer by 8.5 to get rough total)
+        //multiple input time by 9 to get rought estimate of minutes (average time is about 7 minutes)
         const newTimer = timer * 9;
-        //invoke aggregate function on database to pull only a size adding up to timer, with data sorted by key
+        //invoke aggregate function on database and assign evaluated result to res.locals.playlist
        res.locals.playlist = await songSchema.aggregate([
+        //pull random amount of songs equal to size of newTimer
         { $sample: { size: newTimer } },
+        //sort in ascending order of key
         { $sort: { key: 1}},
       ]);
         return next()
@@ -19,16 +20,9 @@ const setController = {
 
 };
 
-//It seems like a filter for adding up certain properties doens't really exist so I might just have to assume that the average length
-    //of each song is about 6 minutes. Then I can divide newTimer by 6 to find out how many songs I should pull to populate the new playlist,
-    //or just make newTimer be timer * 10 instead. Still need to figure out how to create random querying 
-
-    //also need to import the database as db so that I can filter the aggregation on db.songs.aggregation
-
-    // { $sort: {key: 1} }    This line should filter the keys in ascending order which is good enough for now
-    // { $sample: {size: 10}} This line with pull 10 random songs from the playlist. Works well but I need the songs to be ordered first
-        //I think, then pull a random number all next to each other. It works better with a bigger sample size because its pulling randomly
-        //so it cant always get something directly in the next key so it skips but thats kind of ok
+ //idea is to pull time from req.body, convert to amount of songs roughly adding up to minutes, then find a random pull of data 
+ //from the database adding up to input time. Once that's done, then the pulled data needs to be sorted into
+ //ascending order by key: 1A -> 1B -> 2A
 
 module.exports = setController;
 
