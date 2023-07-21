1897ce6a7fbc9bfe1e8dd908312c0c89f96aa1dc
HShaw215 <hu.shaw215@gmail.com>
2023-07-21 16:08:39 -0400

    configured loginPage structure

---
+++ client/components/loginContainer/loginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../scss/containerStyles.scss'
+import { useAppDispatch, useAppSelector } from '../store/hooks.js'
+
+function Login() {
+
+    const dispatch = useAppDispatch();
+    
+    return (
+        <div>
+    
+        </div>
+    );
+}
+
+export default Login;
