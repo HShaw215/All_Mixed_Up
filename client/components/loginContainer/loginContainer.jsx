ff98e075afff8145ce766f7811f984a98dddba6a
HShaw215 <hu.shaw215@gmail.com>
2023-07-24 15:04:53 -0400

    configured state tracking for username and pass

--- client/components/loginContainer/loginContainer.jsx
+++ client/components/loginContainer/loginContainer.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../scss/containerStyles.scss'
 import { useAppDispatch, useAppSelector } from '../../store/hooks.js'
 
 function LoginContainer() {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     const dispatch = useAppDispatch();
     
     return (
         <div className='box'>
-            <input placeholder='Username'></input>
-            <input placeholder='Password'></input>
-           <button type='submit'></button>
+            <input placeholder='Username'  onChange = {e => setUsername(e.target.value)}></input>
+            <input placeholder='Password'  onChange = {e => setPassword(e.target.value)}></input>
+           <button type='submit' id='loginButton' onClick = {() => console.log('Logging In', username, password)} >Submit</button>
         </div>
     );
 }
