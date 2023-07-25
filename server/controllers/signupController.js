828772f9e9f90e62e710f7a78532da565306fcf6
HShaw215 <hu.shaw215@gmail.com>
2023-07-25 16:15:33 -0400

    configured layout of loginController

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -5,7 +5,7 @@ const signupController = {
     //declare method createUser that creates a new user in database 
     async createUser(req, res, next) {
         
-        // return next()
+        return next()
     }
 
 };
