9ad75211c2b02ea404829ce90e5a5d89c7d5692c
HShaw215 <hu.shaw215@gmail.com>
2023-07-25 16:14:54 -0400

    configured layout of loginController

---
+++ server/controllers/loginController.js
@@ -0,0 +1,14 @@
+const songSchema = require('../models/songModel');
+
+const loginController = {
+
+    //declare method verifyInfo that checks if input password matches database password to login user
+    async verifyInfo(req, res, next) {
+        
+        return next()
+    }
+
+};
+
+
+module.exports = loginController;
