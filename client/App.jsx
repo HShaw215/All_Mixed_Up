4adbe3168367b5e97ac3aa494b9c31420d72797b
HShaw215 <hu.shaw215@gmail.com>
2023-07-25 15:40:00 -0400

    added route for signup page

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainContainer from './components/MainContainer.jsx';
-import LoginContainer from './components/loginContainer/loginContainer.jsx'
+import LoginContainer from './components/loginContainer/loginContainer.jsx';
+import SignUpContainer from './components/loginContainer/SignUpContainer.jsx'
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
     <Routes>
         <Route path='/' element={<LoginContainer />} />
+        <Route path='/' element={<SignUpContainer />} />
         <Route path='/main' element={<MainContainer />} />
     </Routes>
 );
