b48af812d72a94c12d95e74bef56c1b6ceeb4ccf
HShaw215 <hu.shaw215@gmail.com>
2023-07-25 16:08:54 -0400

    created loginRoute

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const loginRoute = express.Router();
+
+//route will verify that login username matches stored password in database
+loginRoute.post('/', loginController.verifyInfo, (req, res) => {
+    console.log('user verified');
+    res.status(200).send(res.locals.status)
+});
+
+module.exports = loginRoute;
