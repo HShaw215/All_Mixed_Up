78c8386a9808688c2bdceb2110ae4a14139daf67
HShaw215 <hu.shaw215@gmail.com>
2023-07-25 15:13:21 -0400

    added navigate elements to signup page

--- client/components/loginContainer/singnupContainer.jsx
+++ client/components/loginContainer/singnupContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../../scss/containerStyles.scss'
+import { Link, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../store/hooks.js'
 import { setUserID } from '../../store/appSlice';
 
@@ -8,6 +9,7 @@ function SignUpContainer() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+     // const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const userID = useAppSelector((state) => state.app.userID);
     const status = false;
@@ -37,7 +39,7 @@ function SignUpContainer() {
             <input className='loginForm' id='username' placeholder='Username' onChange = {e => setUsername(e.target.value)}></input>
             <input className='loginForm' id='password' type='password' placeholder='Password' onChange = {e => setPassword(e.target.value)}></input>
            <button type='submit' id='loginButton' onClick = {() => signupSubmit(username, password)} >Submit</button>
-           <a class='login' href='/login'>Login</a>
+           <a class='createAccount' href='/login'>Login</a>
         </div>
     );
 }
