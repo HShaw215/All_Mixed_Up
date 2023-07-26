c36a75f4920716cfd7a5501183e66804efffd729
HShaw215 <hu.shaw215@gmail.com>
2023-07-26 13:52:38 -0400

    added navBar to loginContainer

--- client/components/loginContainer/loginContainer.jsx
+++ client/components/loginContainer/loginContainer.jsx
@@ -36,11 +36,16 @@ function LoginContainer() {
     };
     
     return (
-        <div className='box'>
-            <input className='loginForm' id='username' placeholder='Username' onChange = {e => setUsername(e.target.value)}></input>
-            <input className='loginForm' id='password' type='password' placeholder='Password' onChange = {e => setPassword(e.target.value)}></input>
-            <button type='submit' id='loginButton' onClick = {() => loginSubmit(username, password)} >Submit</button>
-            <Link className="createAccount" to='/signup' >Create an account</Link>
+        <div>
+            <nav className='navBar'>
+                <p>All Mixed Up</p>
+            </nav>
+            <div className='box'>
+                <input className='loginForm' id='username' placeholder='Username' onChange = {e => setUsername(e.target.value)}></input>
+                <input className='loginForm' id='password' type='password' placeholder='Password' onChange = {e => setPassword(e.target.value)}></input>
+                <button type='submit' id='loginButton' onClick = {() => loginSubmit(username, password)} >Submit</button>
+                <Link className="createAccount" to='/signup' >Create an account</Link>
+            </div>
         </div>
     );
 }
