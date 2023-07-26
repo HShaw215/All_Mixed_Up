8f4fc49a9b0b901c782ed020b251317fd8288aba
HShaw215 <hu.shaw215@gmail.com>
2023-07-26 14:14:46 -0400

    created userSchema in database model

--- server/controllers/songController.js
+++ server/controllers/songController.js
@@ -1,4 +1,4 @@
-const Song = require('../models/songModel');
+const { Song } = require('../models/songModel')
 
 const songController = {
 
--- server/models/songModel.js
+++ server/models/songModel.js
@@ -4,10 +4,27 @@ const Schema = mongoose.Schema;
 const mongoURI = "mongodb+srv://hshaw:trial1337@soloproject.k5xsniw.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoURI)
 
+const userSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+});
+
+userSchema.pre('save', function(next){
+    bcrypt.hash(this.password, SALT_WORK_FACTOR, (err, hash) => {
+        if (err) return next(err)
+        this.password = hash;
+        return next();
+    })
+});
+
+const User = mongoose.model('user', userSchema)
+
 const songSchema = new Schema({
     name : {type: String, required: true, unique: true},
     key: {type: String, required: true},
     length: {type: Number, required: true},
 });
 
-module.exports = mongoose.model('Song', songSchema);
+const Song = mongoose.model('Song', songSchema)
+
+module.exports = { User, Song };
