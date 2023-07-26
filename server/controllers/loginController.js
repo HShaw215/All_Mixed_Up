1972f20a61ea0c425c353fbb7092b1f341dbfb7b
HShaw215 <hu.shaw215@gmail.com>
2023-07-26 14:35:55 -0400

    configured logic for createUser controller

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -4,15 +4,24 @@ const loginController = {
 
     //declare method verifyInfo that checks if input password matches database password to login user
     async verifyInfo(req, res, next) {
-        
-        return next()
+        console.log('verifyInfo controller running')
+       
     },
 
 
     //declare method createUser that creates a new user in database 
     async createUser(req, res, next) {
-       
-        return next()
+        console.log('createUser controller running')
+        const { username, password } = req.body;
+        try{
+            res.locals.user = User.create( { username, password } );
+            return next()
+        }
+       catch{
+        return next({
+            err: {err : 'Username already taken'}
+        });
+       }
     }
     
 
