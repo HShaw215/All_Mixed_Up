86cd6b0b2348cbbc3e17dff1713b3db0d6184ad8
HShaw215 <hu.shaw215@gmail.com>
2023-07-26 14:36:25 -0400

    configured verifyInfo controller

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -5,7 +5,27 @@ const loginController = {
     //declare method verifyInfo that checks if input password matches database password to login user
     async verifyInfo(req, res, next) {
         console.log('verifyInfo controller running')
-       
+        const { username, password } = req.body;
+        try{
+            const user = User.findOne( { username } );
+            if (!user){
+                res.locals.status = false;
+                return next();
+            } else {
+                const check = bcrypt.compare(password, user.password);
+                if (!check){
+                    res.locals.status = false;
+                } else {
+                    res.locals.user = user.id;
+                    return next()
+                }
+            }
+        }
+        catch {
+            return next({
+                err: {err : 'Invalid username or password'}
+            });
+        }
     },
 
 
