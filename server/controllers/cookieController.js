ddc01218a225d5c5a1105168f8b223019187ccbc
HShaw215 <hu.shaw215@gmail.com>
2023-07-31 13:54:22 -0400

    completed setSSIDCookie controller

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -6,7 +6,19 @@ const cookieController = {
         return next();
     },
 
-
+    setSSIDCookie(req, res, next) {
+        try{
+            res.cookie('ssid', res.locals.user, { httpOnly: true });
+            return next()
+        }
+        catch {
+            return next({
+                log: `Error occured in setSSIDCookie controller: ${err}`,
+                status: 400,
+                message: { err: "An error occurred" },
+            });
+        }
+    }
 
 };
 
