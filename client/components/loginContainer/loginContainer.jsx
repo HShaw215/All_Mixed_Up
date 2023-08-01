1e99a68093f1ca8d329e745acd2a0fdd532fd115
HShaw215 <hu.shaw215@gmail.com>
2023-08-01 14:02:54 -0400

    altered loginContainer fetch

--- client/components/loginContainer/loginContainer.jsx
+++ client/components/loginContainer/loginContainer.jsx
@@ -3,39 +3,51 @@ import '../../scss/containerStyles.scss'
 import { useNavigate } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
 import { useAppDispatch, useAppSelector } from '../../store/hooks.js'
-import { setUserID } from '../../store/appSlice';
+import { setUserID, setLoggedIn } from '../../store/appSlice';
 
 function LoginContainer() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const userID = useAppSelector((state) => state.app.userID);
-    let status = false;
 
     const loginSubmit = async (username, password) => {
         console.log('login form submitting');
+        const usernames = (document.getElementById('username')).value;
+        const passwords = (document.getElementById('password')).value
         dispatch(setUserID(username));
-        await fetch('/api/user/login', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({username, password})
-        }).then(response =>{
-            status = response;
+        console.log(userID)
+        console.log(usernames, passwords)
+        try{
+                const response = await fetch('/api/user/login', {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ username: username, password: password })
+            });
+            if (response){
+                dispatch(setLoggedIn(true));
+                navigate('/main')
+            }
+            else alert('Invalid Username or Password')
+        }
+        catch(err) {
+            console.log('Error:', err)
+        }
+       
             // if (status) navigate('/main')
             // else alert('Invalid Username or Password')
-        });
+            document.getElementById('username').value = '';
+            document.getElementById('password').value = '';
+        };
 
         // console.log(username, password)
         // console.log('userID:', userID)
 
-        document.getElementById('username').value = '';
-        document.getElementById('password').value = '';
-    };
     
     return (
         <div>
