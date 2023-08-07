fa791f39aee35f95b24744b300417870ce7f6e46
HShaw215 <hu.shaw215@gmail.com>
2023-08-07 12:25:07 -0400

    completed signupform functionality

--- client/components/loginContainer/SignUpContainer.jsx
+++ client/components/loginContainer/SignUpContainer.jsx
@@ -3,35 +3,57 @@ import '../../scss/containerStyles.scss'
 import { useNavigate } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
 import { useAppDispatch, useAppSelector } from '../../store/hooks.js'
-import { setUserID } from '../../store/appSlice';
+import { setUserID, setLoggedIn } from '../../store/appSlice';
 
 function SignUpContainer() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-     // const navigate = useNavigate();
+     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const userID = useAppSelector((state) => state.app.userID);
     let status = false;
 
     const signupSubmit = async (username, password) => {
         console.log('sign up form submitting');
-        await fetch('/api/user/signup', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin" : "*",
-            },
-            body: JSON.stringify({username, password})
-        }).then(response => status = response)
-        
-        if (status === true){
-            dispatch(setLoggedIn(true));
-            dispatch(setUserID(username));
-            navigate('/main')
+        try {
+            const response = await fetch('/api/user/signup', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin" : "*",
+                },
+                body: JSON.stringify({username, password})
+            });
+            if (!response.ok){
+                throw new Error ('Invalid Username or Password')
+            }
+            else {
+                dispatch(setLoggedIn(true));
+                dispatch(setUserID(username));
+                navigate('/main');
+            }
         }
-        else alert('Invalid Username or Password')
+        catch(err){
+            console.log('Error:', err)
+        };
+       
+        //     if (response === true){
+        //         console.log(status)
+        //         dispatch(setLoggedIn(true));
+        //         dispatch(setUserID(username));
+        //         navigate('/main')
+        //     }
+        //     else alert('Invalid Username or Password')
+        // })
+        
+        // if (status === true){
+        //     dispatch(setLoggedIn(true));
+        //     dispatch(setUserID(username));
+        //     navigate('/main')
+        // }
+        // else alert('Invalid Username or Password')
 
         document.getElementById('username').value = '';
         document.getElementById('password').value = '';
