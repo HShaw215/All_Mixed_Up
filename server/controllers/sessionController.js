2ea8b4ca951c1129ddb674ac7d9f1b08243fcf75
HShaw215 <hu.shaw215@gmail.com>
2023-08-07 12:47:14 -0400

    created startSession controller

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -5,9 +5,8 @@ const sessionController = {
     async isLoggedIn(req, res, next){
         console.log('isLoggedIn controller is running');
         console.log('req.cookies:', req.cookies);
-        
         try{
-            const session = await Session.findOne({ cookieId: req.cookies.ssid });
+            const session = await Session.findOne({ cookieID: req.cookies.ssid });
 
             if (!session){
                 return res.send(false)
@@ -19,9 +18,28 @@ const sessionController = {
                 log: `Error occured in isLoggedIn controller: ${err}`,
                 status: 400,
                 message: { err: "An error occurred" },
-            })
-        }
+            });
+        };
     },
+
+    async startSession(req, res, next){
+        console.log('startSession controller is running');
+        console.log('user:', res.locals.user);
+        try{
+            await Session.findOneAndUpdate(
+                { cookieID: res.locals.user },
+                { createdAt: Date.now() },
+                { upsert: true, setDefaultsOnInsert: true }
+            );
+            return next()
+        } catch(err){
+            return next({
+                log: `Error occured in startSession controller: ${err}`,
+                status: 400,
+                message: { err: "An error occurred" },
+            });
+        };
+    }
 
 };
 
