e9c835863401dc624d02b0b852e37887e939eee2
HShaw215 <hu.shaw215@gmail.com>
2023-08-07 12:49:19 -0400

    added cookie and session checks to loginRoute

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const loginController = require('../controllers/loginController')
+const cookieController = require('../controllers/cookieController')
+const sessionController = require('..//controllers/sessionController')
 
 const loginRoute = express.Router();
 
 //route will verify that login username matches stored password in database
-loginRoute.post('/login', loginController.verifyInfo, (req, res) => {
+loginRoute.post('/login', loginController.verifyInfo, sessionController.startSession, cookieController.setSSIDCookie, (req, res) => {
     console.log('user verified');
     res.status(200).send(res.locals.status)
 });
 
-loginRoute.post('/signup', loginController.createUser, (req, res) => {
+loginRoute.post('/signup', loginController.createUser, sessionController.startSession, cookieController.setSSIDCookie, (req, res) => {
     console.log('user created');
     res.status(201).send(res.locals.status)
 });
