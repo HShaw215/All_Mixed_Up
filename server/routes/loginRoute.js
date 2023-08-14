cab4cd5144dde2b6b9de97d07b8572e2b92b669e
HShaw215 <hu.shaw215@gmail.com>
2023-08-14 15:22:04 -0400

    created logout route

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -6,14 +6,21 @@ const sessionController = require('..//controllers/sessionController')
 const loginRoute = express.Router();
 
 //route will verify that login username matches stored password in database
-loginRoute.post('/login', loginController.verifyInfo, sessionController.startSession, cookieController.setSSIDCookie, (req, res) => {
+loginRoute.post('/login', loginController.verifyInfo, sessionController.startSession, cookieController.setSSIDCookie, cookieController.setCookie, (req, res) => {
     console.log('user verified');
     res.status(200).send(res.locals.status)
 });
 
-loginRoute.post('/signup', loginController.createUser, sessionController.startSession, cookieController.setSSIDCookie, (req, res) => {
+//route will create user in database and log them in
+loginRoute.post('/signup', loginController.createUser, sessionController.startSession, cookieController.setSSIDCookie, cookieController.setCookie, (req, res) => {
     console.log('user created');
     res.status(201).send(res.locals.status)
+});
+
+//route will log user out by clearing session cookie and redirecting to login page
+loginRoute.get('/logout', (req, res) => {
+    console.log('user logged out');
+    return res.clearCookie('ssid').redirect('/')
 });
 
 module.exports = loginRoute;
