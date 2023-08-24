dfe15e0a5fdeb9dcb7aefd79b77827623acfb032
HShaw215 <hu.shaw215@gmail.com>
2023-08-24 14:24:37 -0400

    added check for isLogged in to save session

--- client/App.jsx
+++ client/App.jsx
@@ -3,13 +3,25 @@ import MainContainer from './components/MainContainer.jsx';
 import LoginContainer from './components/loginContainer/loginContainer.jsx';
 import SignUpContainer from './components/loginContainer/SignUpContainer.jsx'
 import { Route, Routes } from 'react-router-dom';
+import { useAppSelector } from './store/hooks.js';
 
-const App = () => (
-    <Routes>
+export default function App(){
+    const loggedIn = useAppSelector((state) => state.app.loggedIn);
+    console.log(loggedIn)
+   return loggedIn ? (
+    <>
+        <Routes>
+        <Route path='/main' element={<MainContainer />} />
+        </Routes>
+    </>
+   ) : (
+    <>
+        <Routes>
         <Route path='/' element={<LoginContainer />} />
         <Route path='/signup' element={<SignUpContainer />} />
         <Route path='/main' element={<MainContainer />} />
-    </Routes>
-);
+        </Routes>
+    </>
+   );
+};
 
-export default App;
