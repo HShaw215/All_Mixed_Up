3b5fdfa6fb328e6e3a38375eb2c91327998ad0e5
HShaw215 <hu.shaw215@gmail.com>
2023-11-21 13:40:31 -0500

    removed cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 const loginRoute = require('./routes/loginRoute');
@@ -12,7 +11,6 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors())
 app.use(cookieParser());
 
 // app.use('/build', express.static(path.resolve(__dirname, '../build')))
