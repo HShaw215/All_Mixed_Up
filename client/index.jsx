7f98e83419e1d94cfda0337960331b867ea6b1fd
HShaw215 <hu.shaw215@gmail.com>
2023-12-11 13:35:49 -0500

    removed hashRouter

--- client/index.jsx
+++ client/index.jsx
@@ -8,10 +8,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 ReactDom.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <BrowserRouter>
-            <HashRouter basename='/'>
                 <App />
-            </HashRouter>
-           
         </BrowserRouter>
     </Provider>
 );
